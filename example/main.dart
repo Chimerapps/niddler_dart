eeda6b14adbfac32c0a9a5d68daca7362616c2e3
Nicola Verbeeck <nicola@chimerapps.com>
2019-07-14 10:21:58 +0200

    Applied dart fmt to example

--- example/main.dart
+++ example/main.dart
@@ -6,8 +6,10 @@ import 'package:niddler_dart/niddler_dart.dart';
 Future<void> main() async {
   final niddlerBuilder = NiddlerBuilder()
     ..bundleId = 'com.test.test'
-    ..serverInfo = NiddlerServerInfo('Some descriptive name', 'Some description')
-    ..port = 0; //0 to have niddler pick it's own port. Automatic discovery will make this visible
+    ..serverInfo =
+        NiddlerServerInfo('Some descriptive name', 'Some description')
+    ..port =
+        0; //0 to have niddler pick it's own port. Automatic discovery will make this visible
 
   final niddler = niddlerBuilder.build()..addBlacklist(RegExp('.*/get'));
   await niddler.start();
@@ -23,9 +25,12 @@ Future<void> main() async {
     ]
   };
 
-  final response = await http.post('http://httpbin.org/post', body: json.encode(value), headers: {'content-type': 'application/json'});
-  final response2 = await http.get('http://httpbin.org/get', headers: {'content-type': 'application/json'});
-  await http.get('https://raw.githubusercontent.com/zynksoftware/samples/master/XML%20Samples/Credit%20note%20with%20Sales%20Payment.xml');
+  final response = await http.post('http://httpbin.org/post',
+      body: json.encode(value), headers: {'content-type': 'application/json'});
+  final response2 = await http.get('http://httpbin.org/get',
+      headers: {'content-type': 'application/json'});
+  await http.get(
+      'https://raw.githubusercontent.com/zynksoftware/samples/master/XML%20Samples/Credit%20note%20with%20Sales%20Payment.xml');
   await http.get('https://www.google.com/'); //Some html!
   await http.get('http://placekitten.com/200/300');
 
